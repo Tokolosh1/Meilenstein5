e83a4db8c66ebb18575edd6a95ae90c065dc1e6b
Tokolosh1 <mein_dackel@web.de>
2012-01-21 17:18:16 +0100

    first commit

--- Meilenstein5/index.aspx.designer.cs
+++ Meilenstein5/index.aspx.designer.cs
@@ -20,14 +20,5 @@ namespace Meilenstein5 {
         /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// Login1-Steuerelement
-        /// </summary>
-        /// <remarks>
-        /// Automatisch generiertes Feld
-        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Login Login1;
     }
 }
